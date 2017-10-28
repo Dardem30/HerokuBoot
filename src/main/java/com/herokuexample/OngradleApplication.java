d201b6df939e7f999803d05552ae5cb1bbe969eb
Dardem30 <r.lukashenko@mail.ru>
2017-10-28 15:20:02 +0300

    geroku

---
+++ src/main/java/com/herokuexample/OngradleApplication.java
@@ -0,0 +1,12 @@
+package com.herokuexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OngradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OngradleApplication.class, args);
+	}
+}
